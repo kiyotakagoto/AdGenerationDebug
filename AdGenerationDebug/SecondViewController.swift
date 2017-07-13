599e38209cdad72ae0b94d600060a07a87d5d433
kiyotakagoto <spain.xavi.h@gmail.com>
2017-07-14 00:04:21 +0900

    Initial Commit

---
+++ AdGenerationDebug/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AdGenerationDebug
+//
+//  Created by 後藤 清豪 on 2017/07/14.
+//  Copyright © 2017年 kiyotakagoto.com. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
